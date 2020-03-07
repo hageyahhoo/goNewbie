1ede877bb945abb0a38e42b7a759dc7f5c803a4b
Hiroyuki Ito <hageyahhoo@gmail.com>
2020-03-07 18:00:26 +0900

    Added custom log feature

---
+++ src/io/customLog.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+)
+
+var LOG_FILE = "/Users/hageyahhoo/repository/src/goNewbie/custom.log"
+
+func main() {
+	f, err := os.OpenFile(LOG_FILE, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		fmt.Println(err.Error())
+		os.Exit(9)
+	}
+	defer f.Close()
+
+	customLog := log.New(f, "CustomLog", log.LstdFlags|log.Lshortfile)
+	//	customLog.Fatal("OH!")
+	//	customLog.Panic("orz")
+	customLog.Println("Los Ingobernables de Japon!")
+}
