19b1cb82849bb4a6ebf29ae605a55f924f37f6e2
Hiroyuki Ito <hageyahhoo@gmail.com>
2020-11-03 16:37:53 +0900

    %s -> %q

--- src/ut/config_test.go
+++ src/ut/config_test.go
@@ -7,7 +7,7 @@ import (
 func TestConfig(t *testing.T) {
 	result := Config()
 	if result != "kubeconfig" {
-		t.Errorf("Failure: kubeconfig is not %s", result)
+		t.Errorf("Failure: kubeconfig is not %q", result)
 	}
 	t.Log("result", result)
 }
