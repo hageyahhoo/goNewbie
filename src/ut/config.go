a995e3d080d864469cda431f8b7d787ab73d054e
Hiroyuki Ito <hageyahhoo@gmail.com>
2020-11-03 17:25:15 +0900

    Added Server() with Parameterized Testing

--- src/ut/config.go
+++ src/ut/config.go
@@ -3,3 +3,13 @@ package ut
 func Config() string {
 	return "kubeconfig"
 }
+
+func Server(key string) string {
+	if key == "kube-apiserver" {
+		return "kube-apiserver"
+	}
+	if key == "kube-controller-manager" {
+		return "kube-controller-manager"
+	}
+	return "kube-scheduler"
+}
--- src/ut/config_test.go
+++ src/ut/config_test.go
@@ -11,3 +11,15 @@ func TestConfig(t *testing.T) {
 	}
 	t.Log("result", result)
 }
+
+func TestServer(t *testing.T) {
+	for key, expected := range map[string]string{
+		"kube-apiserver":          "kube-apiserver",
+		"kube-controller-manager": "kube-controller-manager",
+		"xxx":                     "kube-scheduler",
+	} {
+		if actual := Server(key); expected != actual {
+			t.Errorf("Failure: expected %q but returned %q", expected, actual)
+		}
+	}
+}
