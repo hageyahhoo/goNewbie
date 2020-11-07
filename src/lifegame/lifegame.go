a7afe4b1030fb65e587cb25a974cc435638f69ed
Hiroyuki Ito <hageyahhoo@gmail.com>
2020-11-07 22:08:29 +0900

    Adding changeWorld() but not working properly

--- src/lifegame/lifegame.go
+++ src/lifegame/lifegame.go
@@ -1,16 +1,77 @@
 package lifegame
 
+import (
+	"fmt"
+)
+
 type World struct {
 	cell [][]bool
+	size int
 }
 
-func createWorld(x, y int) World {
-	w := new(World)
+func createWorld(initialState [][]bool) World {
+	world := new(World)
 
-	w.cell = make([][]bool, x)
-	for i := 0; i < x; i++ {
-		w.cell[i] = make([]bool, y)
+	world.size = len(initialState)
+	world.cell = make([][]bool, world.size)
+	for i := 0; i < world.size; i++ {
+		world.cell[i] = make([]bool, world.size)
+		for j := 0; j < world.size; j++ {
+			world.cell[i][j] = initialState[i][j]
+		}
 	}
 
-	return *w
+	return *world
+}
+
+func changeWorld(world World) {
+	nextState := make([][]bool, world.size)
+	for i := 0; i < world.size; i++ {
+		world.cell[i] = make([]bool, world.size)
+	}
+
+	for i := 0; i < world.size; i++ {
+		for j := 0; j < world.size; j++ {
+			// TODO Refactor
+			count := countNeighbors(world, i, j)
+			if world.cell[i][j] {
+				if count != 3 {
+					nextState[i][j] = false
+				}
+			} else {
+				if count == 2 || count == 3 {
+					nextState[i][j] = true
+				}
+			}
+		}
+	}
+	// FIXME
+	world.cell = nextState
+	fmt.Println(nextState)
+}
+
+func countNeighbors(world World, x, y int) int {
+	var count int
+	for i := max(x - 1, 0); i < min(world.size, x + 1); i++ {
+		for j := max(y - 1, 0); j < min(world.size, y + 1); j++ {
+			if world.cell[i][j] {
+				count++
+			}
+		}
+	}
+	return count
+}
+
+func max(a, b int) int {
+	if a >= b {
+		return a
+	}
+	return b
+}
+
+func min(a, b int) int {
+	if a <= b {
+		return a
+	}
+	return b
 }
--- src/lifegame/lifegame_test.go
+++ src/lifegame/lifegame_test.go
@@ -4,19 +4,44 @@ import (
 	"testing"
 )
 
+// TODO print as string
 func TestCreateWorld(t *testing.T) {
-	// TODO Change param from indices to state
-	world := createWorld(3, 3)
-	expected := [3][3]bool {
-		{ false, false, false },
-		{ false, false, false },
-		{ false, false, false },
+	initialState := [][]bool {
+		{ false, true, false },
+		{ false, true, false },
+		{ false, true, false },
 	}
+	world := createWorld(initialState)
 
-	for i, cells := range expected {
+	for i, cells := range initialState {
 		for j, cell := range cells {
 			if world.cell[i][j] != cell {
-				t.Errorf("Cell error: [%d][%d]", i, j)
+				t.Errorf("Cell error: [%d][%d] != %t", i, j, cell)
+			}
+		}
+	}
+}
+
+
+func TestChangeWorld(t *testing.T) {
+	initialState := [][]bool {
+		{ false, true, false },
+		{ false, true, false },
+		{ false, true, false },
+	}
+	world := createWorld(initialState)
+
+	changeWorld(world)
+
+	nextState := [][]bool {
+		{ false, false, false },
+		{ true, true, true },
+		{ false, false, false },
+	}
+	for i, cells := range nextState {
+		for j, cell := range cells {
+			if world.cell[i][j] != cell {
+				t.Errorf("Cell error: [%d][%d] != %t", i, j, cell)
 			}
 		}
 	}
